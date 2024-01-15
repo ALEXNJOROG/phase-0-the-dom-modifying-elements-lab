53196c6fcae3a46c5bca5f45249321fa1df558b7
Alex Njoroge <alex.njoroge@student.moringaschool.com>
2024-01-15 15:37:00 -0800

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+let mainElement = document.getElementById('main');
+
+mainElement.parentNode.removeChild(mainElement);
+
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = ('is the champion');
+
+document.body.appendChild(newHeader);
+
+
+
